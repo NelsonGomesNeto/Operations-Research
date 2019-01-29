5c21e9b9a682784e6aa55b258ba08564d1af02aa
NelsonGomesNeto <ngn@ic.ufal.br>
2019-01-29 20:47:14 -0300

    Removes '!=' operator from MinimumCostEdgeColoringProblem

--- HardProblems/MinimumCostEdgeColoringProblem/minimumCostEdgeColoringProblem.cpp
+++ HardProblems/MinimumCostEdgeColoringProblem/minimumCostEdgeColoringProblem.cpp
@@ -10,7 +10,7 @@ int main()
   IloCplex cplex(minimumCostEdgeColoringProblem);
 
   // Statement Data:
-    int v, e; scanf("%d %d", &v, &e);
+    int v, e, maxColors; scanf("%d %d", &v, &e); maxColors = (e >> 1) + (e & 1);
     vector<pair<int, int>> graph[v], simplefiedGraph[v]; // {vertex, edge number}
     for (int i = 0, u, v; i < e; i ++)
     {
@@ -19,32 +19,51 @@ int main()
     }
 
   // Decision Variables:
-    IloIntVarArray edgeColor(env, e, 1, e);
+    IloArray<IloBoolVarArray> edgesColors(env, e);
+    for (int i = 0; i < e; i ++) edgesColors[i] = IloBoolVarArray(env, maxColors);
 
   // Restrictions:
+    // Each edge can only have a single color:
+      for (int i = 0; i < e; i ++)
+      {
+        IloExpr sum(env);
+        for (int c = 0; c < maxColors; c ++) sum += edgesColors[i][c];
+        minimumCostEdgeColoringProblem.add(sum == 1);
+      }
     // Adjacent edges must have different colors:
-      IloExpr sameColorCount(env);
       for (int i = 0; i < v; i ++)
         for (int j = 0; j < graph[i].size(); j ++)
           for (int k = j + 1; k < graph[i].size(); k ++)
-            minimumCostEdgeColoringProblem.add(edgeColor[graph[i][j].second] - edgeColor[graph[i][k].second] != 0);
+            for (int c = 0; c < maxColors; c ++)
+              minimumCostEdgeColoringProblem.add(edgesColors[graph[i][j].second][c] + edgesColors[graph[i][k].second][c] <= 1);
 
   // Objective Function:
     IloExpr coloringCost(env);
-    for (int i = 0; i < e; i ++) coloringCost += edgeColor[i];
+    for (int i = 0; i < e; i ++)
+      for (int c = 0; c < maxColors; c ++)
+        coloringCost += (c + 1) * edgesColors[i][c];
     minimumCostEdgeColoringProblem.add(IloMinimize(env, coloringCost));
 
   // Get Solution:
     cplex.solve();
     printf("MinimumColoringCost: %.0lf\n", cplex.getObjValue());
-    IloNumArray edgeColorSolution(env, e); cplex.getValues(edgeColorSolution, edgeColor);
+    IloArray<IloNumArray> edgesColorsSolution(env, e);
+    for (int i = 0; i < e; i ++)
+    {
+      edgesColorsSolution[i] = IloNumArray(env, maxColors);
+      cplex.getValues(edgesColorsSolution[i], edgesColors[i]);
+    }
     for (int i = 0; i < v; i ++)
     {
       printf("%d:", i + 1);
-      for (auto j: simplefiedGraph[i]) printf(" (%d, %d)", j.first + 1, (int) edgeColorSolution[j.second]);
+      for (auto j: simplefiedGraph[i])
+      {
+        int chosenColor = -1;
+        for (int c = 0; c < maxColors; c ++) if (edgesColorsSolution[j.second][c]) chosenColor = c + 1;
+        printf(" (%d, %d)", j.first + 1, chosenColor);
+      }
       printf("\n");
     }
-        
   
   env.end();
   return(0);
