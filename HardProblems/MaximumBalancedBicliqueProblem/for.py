361fa882e6cd5ba8ab074d1ab1f55fdc895532a2
NelsonGomesNeto <ngn@ic.ufal.br>
2019-01-29 22:43:04 -0300

    Adds test case generator for MaximumBalancedBicliqueProblem

--- HardProblems/MaximumBalancedBicliqueProblem/for.py
+++
@@ -1,5 +0,0 @@
-import os
-
-for i in range(1, 20 + 1):
-    os.system("python3 ./generator.py > testCases/%d.in" % i)
-    os.system("./maximumBalancedBicliqueProblem.run < testCases/%d.in > testCases/%d.out" % (i, i))
