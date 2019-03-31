a3bb4a97fe17c6c76fe31f69509e53b494a7b75a
NelsonGomesNeto <ngn@ic.ufal.br>
2019-03-31 08:17:49 -0300

    Interesting optimizations on Sudoku Generator trought DFS

--- Article/Sudoku.cpp
+++ Article/Sudoku.cpp
@@ -4,6 +4,7 @@
 #include <math.h>
 #include <random>
 #include <time.h>
+#include <algorithm>
 
 Sudoku::Sudoku(int sqrtSize) {
   this->sqrtSize = sqrtSize;
@@ -45,14 +46,30 @@ Sudoku::Sudoku(int sqrtSize) {
     }
   }
 
+  this->visited = new int*[size];
+  for (int i = 0; i < size; i ++) {
+    this->visited[i] = new int[size];
+    for (int j = 0; j < size; j ++) this->visited[i][j] = 0;
+  }
+
+  this->numbersCount = new pair<int, int>[size + 1]; this->numbersCount[0] = {0, 0};
+  for (int i = 1; i <= size; i ++) this->numbersCount[i] = {-size, i};
+
   srand(time(NULL));
 }
 
 void Sudoku::printBoard() {
   int digits = 0, aux = this->size; while (aux) aux /= 10, digits ++;
   for (int i = 0; i < this->size; i ++)
+  {
     for (int j = 0; j < this->size; j ++)
+    {
       printf("%*d%c", digits, this->board[i][j], j < this->size - 1 ? ' ' : '\n');
+      if (j > 0 && j < this->size - 1 && (j + 1) % this->sqrtSize == 0) printf("| ");
+    }
+    if (i > 0 && i < this->size - 1 && (i + 1) % this->sqrtSize == 0)
+      for (int k = 0, end = this->size * (digits + 1) + (this->sqrtSize - 1)*2; k < end; k ++) printf("%c", k < end - 1 ? '-' : '\n');
+  }
 }
 
 void Sudoku::printGrids() {
@@ -62,20 +79,86 @@ void Sudoku::printGrids() {
   // TODO
 }
 
-bool Sudoku::generatePuzzle(int i, int j) {
+void Sudoku::fillDiagonals() {
+  for (int i = 1; i <= this->size; i ++) this->numbersCount[i] = {-this->size, i};
+  for (int i = 0; i < this->size; i ++) for (int j = 1; j <= this->size; j ++) this->lines[i][j] = this->columns[i][j] = false, this->board[i][j - 1] = this->visited[i][j - 1] = 0;
+  for (int i = 0; i < this->sqrtSize; i ++) for (int j = 0; j < this->sqrtSize; j ++) for (int k = 1; k <= this->size; k ++) this->grids[i][j][k] = false;
+
+  for (int k = 0; k < this->sqrtSize; k ++)
+    for (int i = 0; i < this->sqrtSize; i ++)
+      for (int j = 0; j < this->sqrtSize; j ++) {
+        int number = rand() % this->size + 1, l = k*this->sqrtSize + i, c = k*this->sqrtSize + j;
+        for (int n = number % this->size + 1; ; n = n % this->size + 1) {
+          if (this->grids[k][k][n]) continue;
+          this->board[l][c] = n;
+          this->lines[l][n] = this->columns[c][n] = this->grids[k][k][n] = true;
+          this->numbersCount[n].first ++;
+          break;
+        }
+      }
+}
+
+int iterations;
+void Sudoku::generatePuzzle() {
+  int r = 0;
+  do {
+    this->fillDiagonals();
+    // printf("Repeating %d\n", r ++);
+    iterations = 0;
+  } while (!this->generatePuzzleDFS());
+  printf("%s\n", this->isValid() ? "Valid" : "Invalid");
+}
+
+// bool generatePuzzleAStar() {
+
+// }
+
+bool Sudoku::generatePuzzleDFS(int i, int j) {
   if (i == this->size) return(true);
+  // if (iterations > 10000000) return(false);
+  if (this->board[i][j]) return(this->generatePuzzleDFS(i + (j + this->sqrtSize == this->size), (j + this->sqrtSize) % this->size));
+  // this->visited[i][j] ++;
+  // if (this->visited[i][j] > 100000) return(false);
+  // iterations ++;
+
   int number = rand() % this->size + 1, ii = i / this->sqrtSize, jj = j / this->sqrtSize;
-  for (int n = this->nextInLines[i][number], a = 0; a < this->size; n = this->nextInLines[i][n], a ++) {
-    if (this->lines[i][n] || this->columns[j][n] || this->grids[ii][jj][n]) continue;
+  int rr = rand() % 100;//((rand() % 10000) > 3000);
+  // double pp = 1.0 - (double) (i*this->size + j) / ((this->size - 1)*(this->size - 1));
+  // int rep = 0 + log2(this->size * pp);
+  int rep;
+  if (rr < 50) rep = 2;
+  else rep = 1;
 
-    int aux = this->nextInLines[i][((n - 1) < 1) ? this->size : n - 1];
+  for (int n = number, a = 0, aa = 0; a < this->size; n = n % this->size + 1, a ++) {
+    if (this->lines[i][n] || this->columns[j][n] || this->grids[ii][jj][n]) continue; else aa ++;
+
     this->board[i][j] = n, this->lines[i][n] = this->columns[j][n] = this->grids[ii][jj][n] = true;
-    // this->nextInLines[i][((n - 1) < 1) ? this->size : n - 1] = n % this->size + 1, this->nextInColumns[j][((n - 1) < 1) ? this->size : n - 1] = n % this->size + 1, this->nextInGrids[ii][jj][((n - 1) < 1) ? this->size : n - 1] = n % this->size + 1;
+    if (this->generatePuzzleDFS(i + (j + 1 == this->size), (j + 1) % this->size)) return(true);
+    this->board[i][j] = 0, this->lines[i][n] = this->columns[j][n] = this->grids[ii][jj][n] = false;
 
-    if (generatePuzzle(i + (j + 1 == this->size), (j + 1) % this->size)) return(true);
-
-    this->board[i][j] = n, this->lines[i][n] = this->columns[j][n] = this->grids[ii][jj][n] = false;
-    // this->nextInLines[i][((n - 1) < 1) ? this->size : n - 1] = this->nextInColumns[j][((n - 1) < 1) ? this->size : n - 1] = this->nextInGrids[ii][jj][((n - 1) < 1) ? this->size : n - 1] = aux;
+    if (aa == rep) return(false);
   }
   return(false);
+}
+
+bool Sudoku::isValid() {
+  bool linesCount[this->size + 1], columnsCount[this->size + 1], gridsCount[this->size + 1];
+  for (int i = 0; i < this->size; i ++) {
+    memset(linesCount, false, sizeof(linesCount)), memset(columnsCount, false, sizeof(columnsCount));
+    for (int j = 0; j < this->size; j ++) {
+      if (linesCount[this->board[i][j]] || columnsCount[this->board[j][i]]) return(false);
+      linesCount[this->board[i][j]] = columnsCount[this->board[j][i]] = true;
+    }
+  }
+  for (int i = 0; i < sqrtSize; i ++)
+    for (int j = 0; j < sqrtSize; j ++) {
+      memset(gridsCount, false, sizeof(gridsCount));
+      for (int k = 0; k < sqrtSize; k ++)
+        for (int m = 0; m < sqrtSize; m ++) {
+          int ii = i*sqrtSize + k, jj = j*sqrtSize + m;
+          if (gridsCount[this->board[ii][jj]]) return(false);
+          gridsCount[this->board[ii][jj]] = true;
+        }
+    }
+  return(true);
 }
--- Article/Sudoku.hpp
+++ Article/Sudoku.hpp
@@ -1,16 +1,24 @@
 #ifndef SUDOKU_HPP
 #define SUDOKU_HPP
 
+#include <utility>
+using namespace std;
+
 class Sudoku {
 private:
   int **board, ****grid, size, sqrtSize;
   bool **lines, **columns, ***grids; // used i-th number
   int **nextInLines, **nextInColumns, ***nextInGrids;
+  int **visited;
+  pair<int, int> *numbersCount; // first numberCount, second number
+  bool generatePuzzleDFS(int i = 0, int j = 0);
+  void fillDiagonals();
 public:
   Sudoku(int sqrtSize);
   void printBoard();
   void printGrids();
-  bool generatePuzzle(int i = 0, int j = 0);
+  void generatePuzzle();
+  bool isValid();
 };
 
 #endif
